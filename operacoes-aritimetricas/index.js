05c8fded7ebf22560a97f46d034fc13d65f7ab46
Maycon Gabriel | Front End Developer <115764624+maycony@users.noreply.github.com>
2024-11-26 19:55:07 -0300

    Add files via upload

---
+++ operacoes-aritimetricas/index.js
@@ -0,0 +1,12 @@
+const a = 10
+const b = 5
+
+const soma = `Soma: ${a} + ${b} = ` + a + b
+const subtracao = `Subtração: ${a} - ${b} = ` + (a - b)
+const multiplicacao = `Multiplicação: ${a} X ${b} = ` + (a * b)
+const divisao = `Divisão: ${a} ÷ ${b} = ` + (a / b)
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
---
+++ operacoes-aritimetricas/readme.md
@@ -0,0 +1,4 @@
+## Operações Aritméticas
+
+Crie duas variáveis a e b. Atribua valores a elas e realize as operações aritméticas 
+básicas (soma, subtração, multiplicação e divisão). Imprima os resultados na tela.
